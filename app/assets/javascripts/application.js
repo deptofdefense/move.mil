fedba31dd54c269f46c45e939405a707a786aea1
Jason Garber <jason@dds.mil>
2017-07-26 12:22:03 -0400

    Add custom carousel assets, styling, and JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,16 @@
 //= require 'uswds/uswds'
+//= require 'jquery'
+//= require 'owl-carousel2'
+
+(function(window, $) {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
+})(window, jQuery);
