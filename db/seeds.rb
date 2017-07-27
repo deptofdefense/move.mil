a1d58f3aebbe41d9438517c63fd012e213699e0a
Jason Garber <jason@dds.mil>
2017-07-27 09:38:35 -0400

    Add tutorials seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tutorials = YAML::load_file(Rails.root.join('db', 'seeds', 'tutorials.yml'))
+
+tutorials.each do |tutorial|
+  record = Tutorial.where(title: tutorial['title']).first_or_create
+
+  tutorial['tutorial_steps'].each do |step|
+    record.tutorial_steps.where(content: step['content']).first_or_create(step)
+  end
+end
