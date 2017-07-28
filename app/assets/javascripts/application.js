a820a9c79f8fda1166f437a76cfe92dd72a53a7d
Jason Garber <jason@dds.mil>
2017-07-28 10:41:58 -0400

    Move carousel JS into its own file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,4 @@
 //= require 'uswds/uswds'
 //= require 'jquery'
 //= require 'owl-carousel2'
-
-(function(window, $) {
-  'use strict';
-
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      }
-    }
-  });
-})(window, jQuery);
+//= require_tree ./components
---
+++ app/assets/javascripts/components/carousel.js
@@ -0,0 +1,14 @@
+(function(window, $) {
+  'use strict';
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
+})(window, jQuery);
