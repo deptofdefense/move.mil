4cd43afc466050f4b996092019b9170a98a479fb
James Athey <james@dds.mil>
2017-08-02 16:13:05 -0400

    DRY - use default_scope to do ordering, and separate scopes for each branch's posts

--- app/controllers/service_specific_information_controller.rb
+++ app/controllers/service_specific_information_controller.rb
@@ -1,15 +1,31 @@
 class ServiceSpecificInformationController < ApplicationController
   def index
-    posts
+    posts_army
+    posts_air_force
+    posts_navy
+    posts_marine_corps
+    posts_coast_guard
   end
 
   private
 
-  def posts
-    @posts_army ||= ServiceSpecificPost.where(branch: 'army').order(:effective_at).reverse_order
-    @posts_air_force ||= ServiceSpecificPost.where(branch: 'air_force').order(:effective_at).reverse_order
-    @posts_marine_corps ||= ServiceSpecificPost.where(branch: 'marine_corps').order(:effective_at).reverse_order
-    @posts_navy ||= ServiceSpecificPost.where(branch: 'navy').order(:effective_at).reverse_order
-    @posts_coast_guard ||= ServiceSpecificPost.where(branch: 'coast_guard').order(:effective_at).reverse_order
+  def posts_army
+    @posts_army ||= ServiceSpecificPost.posts_army
+  end
+
+  def posts_air_force
+    @posts_air_force ||= ServiceSpecificPost.posts_air_force
+  end
+
+  def posts_navy
+    @posts_navy ||= ServiceSpecificPost.posts_navy
+  end
+
+  def posts_marine_corps
+    @posts_marine_corps ||= ServiceSpecificPost.posts_marine_corps
+  end
+
+  def posts_coast_guard
+    @posts_coast_guard ||= ServiceSpecificPost.posts_coast_guard
   end
 end
