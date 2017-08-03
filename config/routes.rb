108aea6eff8aa7842ffc8187796527a7a1ed0eac
Jason Garber <jason@dds.mil>
2017-08-03 16:13:44 -0400

    PR #51 feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :faqs, only: :index
-  get '/service-specific-information', to: 'service_specific_information#index', as: 'service_specific_information'
   resources :tutorials, only: :index
+
+  get '/service-specific-information', to: 'service_specific_information#index', as: 'service_specific_information'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
