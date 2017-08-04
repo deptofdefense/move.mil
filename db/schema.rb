464179df7746736888f0649bc4b1a84862012e1a
Jason Garber <jason@dds.mil>
2017-08-04 16:04:51 -0400

    Add slug column to tutorials and FriendlyId initializer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801135107) do
+ActiveRecord::Schema.define(version: 20170804161245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170801135107) do
     t.text "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "slug", null: false
   end
 
   add_foreign_key "tutorial_steps", "tutorials"
