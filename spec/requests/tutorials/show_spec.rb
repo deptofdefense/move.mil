20619b75619aa491bd60f8a6618ba18de18bdb75
Jason Garber <jason@dds.mil>
2017-08-07 13:41:53 -0400

    Add navigation higlight assertion

--- spec/requests/tutorials/show_spec.rb
+++ spec/requests/tutorials/show_spec.rb
@@ -19,6 +19,10 @@ RSpec.describe TutorialsController, type: :request do
       assert_template 'show'
     end
 
+    it 'highlights the Tutorials navigation item' do
+      assert_select '.usa-sidenav-list .usa-current', text: 'Tutorials'
+    end
+
     it 'displays the tutorial' do
       assert_select '.usa-layout-docs-main_content h1', text: 'Test Tutorial Title'
 
