39e432a7730d14969c465f250d45d653d121d1fe
Jason Garber <jason@dds.mil>
2017-08-07 09:25:17 -0400

    Reorganize basic page verification specs into a context

--- spec/requests/tutorials_spec.rb
+++ spec/requests/tutorials_spec.rb
@@ -1,15 +1,21 @@
 RSpec.describe TutorialsController, type: :request do
   describe 'GET #index' do
-    it 'returns HTTP success status code' do
-      get '/tutorials'
+    context 'when navigating to the page' do
+      before do
+        get '/tutorials'
+      end
 
-      expect(response).to have_http_status(:success)
-    end
+      it 'returns HTTP success status code' do
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'renders the index template' do
-      get '/tutorials'
+      it 'renders the index template' do
+        assert_template 'index'
+      end
 
-      assert_template 'index'
+      it 'highlights the Tutorials navigation item' do
+        assert_select '.usa-sidenav-list .usa-current', text: 'Tutorials'
+      end
     end
 
     context 'when tutorials exist' do
