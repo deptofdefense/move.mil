d188833391d47388d0e3e916ea19871c7b1e9b3e
Jason Garber <jason@dds.mil>
2017-08-14 16:43:12 -0400

    Refactor entitlements validations

--- app/models/entitlement.rb
+++ app/models/entitlement.rb
@@ -4,5 +4,6 @@ class Entitlement < ApplicationRecord
   friendly_id :rank
 
   validates :rank, :total_weight_self, presence: true
-  validates :total_weight_self, :total_weight_self_plus_dependents, :pro_gear_weight, :pro_gear_weight_spouse, numericality: { only_integer: true }
+  validates :total_weight_self, numericality: { only_integer: true }
+  validates :total_weight_self_plus_dependents, :pro_gear_weight, :pro_gear_weight_spouse, allow_blank: true, numericality: { only_integer: true }
 end
