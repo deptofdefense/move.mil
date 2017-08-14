ce70c1a54126b63a3078b3f80a1c4f3daca399db
Jason Garber <jason@dds.mil>
2017-08-14 13:45:11 -0400

    Add entitlements#show route and rework FAQs route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :entitlements, only: :index
-  resources :faqs, only: :index
+  resources :entitlements, only: [:index, :show]
   resources :tutorials, only: [:index, :show]
 
   get '/customer-service', to: 'customer_service#index', as: 'customer_service'
+  get '/faqs', to: 'faqs#index', as: 'faqs'
   get '/service-specific-information', to: 'service_specific_information#index', as: 'service_specific_information'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
