7454c4757d26c0fcdc8d03184890771581e027e7
Jason Garber <jason@dds.mil>
2017-09-05 10:26:24 -0400

    Add throwaway JS to handle '#TODO' anchors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,14 @@
 //= require 'jquery'
 //= require 'owl-carousel2'
 //= require_tree ./components
+
+(function($) {
+  'use strict';
+
+  // TODO: Remove this once all '#TODO' links are gone
+  $('a[href="#TODO"]').on('click', function(event) {
+    event.preventDefault();
+
+    alert('Coming soon!');
+  });
+})(jQuery);
