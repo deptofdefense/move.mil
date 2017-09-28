927376e7d68b0ac76d26330c34b987cd8895392c
James Athey <james@dds.mil>
2017-09-28 10:23:23 -0400

    Incorporating PR feedback

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,35 +1,5 @@
 class FaqsController < ApplicationController
   def index
-    faqs
-  end
-
-  private
-
-  def faqs_before_you_move
-    @faqs_before_you_move ||= Faq.faqs_before_you_move
-  end
-
-  def faqs_moving_day
-    @faqs_moving_day ||= Faq.faqs_moving_day
-  end
-
-  def faqs_travel_tips
-    @faqs_travel_tips ||= Faq.faqs_travel_tips
-  end
-
-  def faqs_delivery
-    @faqs_delivery ||= Faq.faqs_delivery
-  end
-
-  def faqs_after_the_move
-    @faqs_after_the_move ||= Faq.faqs_after_the_move
-  end
-
-  def faqs
-    faqs_before_you_move
-    faqs_moving_day
-    faqs_travel_tips
-    faqs_delivery
-    faqs_after_the_move
+    @faqs = Faq.all.group_by(&:category)
   end
 end
--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,11 +1,3 @@
 class Faq < ApplicationRecord
-  validates :question, presence: true
-  validates :answer, presence: true
-  validates :category, presence: true
-
-  scope :faqs_before_you_move, -> { where(category: 'before-you-move') }
-  scope :faqs_moving_day, -> { where(category: 'moving-day') }
-  scope :faqs_travel_tips, -> { where(category: 'travel-tips') }
-  scope :faqs_delivery, -> { where(category: 'delivery') }
-  scope :faqs_after_the_move, -> { where(category: 'after-the-move') }
+  validates :question, :answer, :category, presence: true
 end
