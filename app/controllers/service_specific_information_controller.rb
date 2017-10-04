a9ddd954d0029aa2ad657423a85304b4233f985f
Jason Garber <jason@dds.mil>
2017-10-04 15:55:00 -0400

    Refactor Service Specific Information model, controller, and view

--- app/controllers/service_specific_information_controller.rb
+++ app/controllers/service_specific_information_controller.rb
@@ -1,31 +1,11 @@
 class ServiceSpecificInformationController < ApplicationController
   def index
-    posts_army
-    posts_air_force
-    posts_navy
-    posts_marine_corps
-    posts_coast_guard
-  end
+    grouped_posts = ServiceSpecificPost.all.group_by(&:branch)
 
-  private
-
-  def posts_army
-    @posts_army ||= ServiceSpecificPost.posts_army
-  end
-
-  def posts_air_force
-    @posts_air_force ||= ServiceSpecificPost.posts_air_force
-  end
-
-  def posts_navy
-    @posts_navy ||= ServiceSpecificPost.posts_navy
-  end
-
-  def posts_marine_corps
-    @posts_marine_corps ||= ServiceSpecificPost.posts_marine_corps
-  end
-
-  def posts_coast_guard
-    @posts_coast_guard ||= ServiceSpecificPost.posts_coast_guard
+    @army_posts = grouped_posts['army']
+    @air_force_posts = grouped_posts['air_force']
+    @navy_posts = grouped_posts['navy']
+    @marine_corps_posts = grouped_posts['marine_corps']
+    @coast_guard_posts = grouped_posts['coast_guard']
   end
 end
--- app/models/service_specific_post.rb
+++ app/models/service_specific_post.rb
@@ -1,11 +1,5 @@
 class ServiceSpecificPost < ApplicationRecord
   validates :title, presence: true
 
-  default_scope { order(:effective_at).reverse_order }
-
-  scope :posts_army, -> { where(branch: 'army') }
-  scope :posts_air_force, -> { where(branch: 'air_force') }
-  scope :posts_navy, -> { where(branch: 'navy') }
-  scope :posts_marine_corps, -> { where(branch: 'marine_corps') }
-  scope :posts_coast_guard, -> { where(branch: 'coast_guard') }
+  default_scope { order(effective_at: :desc) }
 end
