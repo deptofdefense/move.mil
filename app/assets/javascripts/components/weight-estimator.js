3569349cff514df91e223aa0456af2a9f123be23
Aileen Chen <aileen@dds.mil>
2017-10-23 17:45:47 -0400

    Calculate subtotal weights JS

--- app/assets/javascripts/components/weight-estimator.js
+++ app/assets/javascripts/components/weight-estimator.js
@@ -2,44 +2,74 @@
   'use strict';
 
   var CoordinatesSearchForm = function(options) {
-    this.$container = options.$container;
-    this.$button = options.$button;
-    this.$quantityInputs = options.$quantityInputs;
+    this.$outerContainer = options.$outerContainer;
+    this.$buttons = options.$buttons;
     this.$weightResult = options.$weightResult;
 
     this.setup();
   };
 
   CoordinatesSearchForm.prototype = {
+    calculateWeight: function($container) {
+      var sum = 0;
+
+      var $quantityInputs = $container.find('.hhg-quantity-input');
+      $quantityInputs.each(function() {
+        var $input = $(this);
+        if ($input.val() > 0) {
+          var $weightInput = $('#' + this.id + '_weight');
+          sum += $input.val() * $weightInput.val();
+        }
+      });
+
+      var $weightInputs = $container.find('.hhg-weight-input');
+      $weightInputs.each(function() {
+        var $input = $(this);
+        if ($input.val() > 0) {
+          sum += parseInt($input.val());
+        }
+      });
+
+      return sum;
+    },
+
     events: {
       click: function(event) {
         event.preventDefault();
 
-        var sum = 0;
-        this.$quantityInputs.each(function() {
-          var $input = $(this);
-          if ($input.val() > 0) {
-            var $weightInput = $('#' + this.id + '_weight');
-            sum += $input.val() * $weightInput.val();
-          }
-        });
+        var $button = $(event.target);
+        var total = this.calculateWeight(this.$outerContainer);
 
-        this.$weightResult.html(sum + " lbs");
+        var isSubtotalButton = $button.hasClass('category-calculate-button');
+        if (isSubtotalButton) {
+          var $categoryContainer = $button.closest('.hhg-category');
+          var subtotal = this.calculateWeight($categoryContainer);
+
+          var categoryKey = $button.attr('id');
+          var $subtotalInput = $categoryContainer.find('#' + categoryKey + '_subtotal');
+          var $totalInput = $categoryContainer.find('#' + categoryKey + '_total');
+
+          $subtotalInput.val(subtotal);
+          $totalInput.val(total);
+        } else {
+          this.$weightResult.html(total + " lbs");
+        }
       }
     },
 
     setup: function() {
-      this.$button.on('click', this.events.click.bind(this));
+      for (var i = 0; i < this.$buttons.length; i++) {
+        $(this.$buttons[i]).on('click', this.events.click.bind(this));
+      }
     }
   };
 
-  var $container = $('#weight-estimator');
+  var $outerContainer = $('#weight-estimator');
 
-  if ($container.length) {
+  if ($outerContainer.length) {
     new CoordinatesSearchForm({
-      $container: $container,
-      $button: $('#calcuate-button'),
-      $quantityInputs: $container.find('.hhg-quantity-input'),
+      $outerContainer: $outerContainer,
+      $buttons: $outerContainer.find('button'),
       $weightResult: $('#weight-result')
     });
   }
