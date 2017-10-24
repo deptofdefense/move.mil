c4731d6ab7303107acd83aadc53f28c2d9fb35b1
Aileen Chen <aileen@dds.mil>
2017-10-24 12:35:05 -0400

    Add missing file

---
+++ spec/factories/household_good_categories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :household_good_category do
+    sequence(:name) { |n| "Category #{n}" }
+    icon 'icons/bedroom.png'
+
+    transient do
+      household_goods_count 3
+    end
+
+    after(:build) do |household_good_category, evaluator|
+      create_list(:household_good, evaluator.household_goods_count, household_good_category: household_good_category)
+    end
+  end
+end
