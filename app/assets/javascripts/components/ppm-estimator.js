02f98d8451c3e0b725e403f2cad35a932476b897
James Athey <james@dds.mil>
2017-11-01 13:39:52 -0400

    AJAX used to load results table and hide form on submit

--- app/assets/javascripts/components/ppm-estimator.js
+++ app/assets/javascripts/components/ppm-estimator.js
@@ -1,6 +1,56 @@
 (function(window, $) {
   'use strict';
 
+  var PpmEstimator = function(options) {
+    this.$alert = options.$alert;
+    this.$form = options.$form;
+    this.$results = options.$results;
+    this.$resultsContainer = options.$resultsContainer;
+
+    this.$form.on('submit', this.events.submit.bind(this));
+  };
+
+  PpmEstimator.prototype = {
+    events: {
+      submit: function(event) {
+        event.preventDefault();
+
+        $.ajax({
+          data: this.$form.serialize(),
+          error: this.handleAjaxError.bind(this),
+          method: this.$form.attr('method'),
+          success: this.handleAjaxSuccess.bind(this),
+          url: this.$form.attr('action')
+        });
+      }
+    },
+
+    handleAjaxError: function() {
+      this.$results.empty().attr('hidden', true);
+      this.$alert.removeAttr('hidden');
+      this.$form.removeAttr('hidden');
+      this.$resultsContainer.trigger('focus');
+    },
+
+    handleAjaxSuccess: function(markup) {
+      this.$alert.attr('hidden', true);
+      this.$form.attr('hidden', true);
+      this.$results.html(markup).removeAttr('hidden');
+      this.$resultsContainer.trigger('focus');
+    }
+  };
+
+  var $ppmEstimateForm = $('#ppm-estimate-form');
+
+  if ($ppmEstimateForm.length) {
+    new PpmEstimator({
+      $alert: $('#ppm-estimate-alert'),
+      $form: $ppmEstimateForm,
+      $results: $('#ppm-estimate-results'),
+      $resultsContainer: $('#ppm-estimate-results-container')
+    });
+  }
+
 })(window, jQuery);
 
 function isDigitKey(evt) {
