36e98009070b237fa77b08fcc59c608b5306ca5f
James Athey <james@dds.mil>
2017-11-07 14:50:04 -0500

    validate weight fields, turn them red if they exceed entitlement

--- app/assets/javascripts/components/ppm-estimator.js
+++ app/assets/javascripts/components/ppm-estimator.js
@@ -48,46 +48,104 @@
   });
 
   var $rank = $('#rank');
-  var $ppmEntitlementWeight = $('#ppm-entitlement-weight');
-  var $ppmEntitlementProgear = $('#ppm-entitlement-progear');
-  var $ppmEntitlementProgearSpouse = $('#ppm-entitlement-progear-spouse');
-  $ppmEntitlementWeight.css('visibility', 'hidden');
-  $ppmEntitlementProgear.css('visibility', 'hidden');
-  $ppmEntitlementProgearSpouse.css('visibility', 'hidden');
+  var $weightAllowanceText = $('#weight-allowance-text');
+  var $progearAllowanceText = $('#progear-allowance-text');
+  var $progearSpouseAllowanceText = $('#progear-spouse-allowance-text');
+  $weightAllowanceText.css('visibility', 'hidden');
+  $progearAllowanceText.css('visibility', 'hidden');
+  $progearSpouseAllowanceText.css('visibility', 'hidden');
   var entitlementsJson = $('#entitlements-json').data('entitlementsJson');
+
+  var getEntitlementWeightSelf = function(entitlement) {
+    return $('#dependents_yes').checked ?
+      (entitlement.total_weight_self_plus_dependents > 0 ? entitlement.total_weight_self_plus_dependents : 0) :
+      (entitlement.total_weight_self > 0 ? entitlement.total_weight_self : 0);
+  };
+
+  var getEntitlementProgear = function(entitlement) {
+    return entitlement.pro_gear_weight > 0 ? entitlement.pro_gear_weight : 0;
+  };
+
+  var getEntitlementProgearSpouse = function(entitlement) {
+    return entitlement.pro_gear_weight_spouse > 0 ? entitlement.pro_gear_weight_spouse : 0;
+  };
 
   var onPersonalInfoChanged = function () {
     if (!$rank.val() || $('input[name="dependents"]:checked').length == 0) {
-      $ppmEntitlementWeight.css('visibility', 'hidden');
-      $ppmEntitlementProgear.css('visibility', 'hidden');
-      $ppmEntitlementProgearSpouse.css('visibility', 'hidden');
+      $weightAllowanceText.css('visibility', 'hidden');
+      $progearAllowanceText.css('visibility', 'hidden');
+      $progearSpouseAllowanceText.css('visibility', 'hidden');
     }
     else {
       var entitlement = entitlementsJson.find(function(data) { return data.slug == $rank.val(); });
 
-      var totalWeightSelf = $('#dependents_yes').checked ?
-          (entitlement.total_weight_self_plus_dependents > 0 ? entitlement.total_weight_self_plus_dependents : 0) :
-          (entitlement.total_weight_self > 0 ? entitlement.total_weight_self : 0);
+      var totalWeightSelf = getEntitlementWeightSelf(entitlement);
       $('#entitlement_weight').text(totalWeightSelf.toString());
-      $ppmEntitlementWeight.css('visibility', 'visible');
+      $weightAllowanceText.css('visibility', 'visible');
+      onWeightInput(null);
 
-      var proGearWeight = entitlement.pro_gear_weight > 0 ? entitlement.pro_gear_weight : 0;
+      var proGearWeight = getEntitlementProgear(entitlement);
       $('#entitlement_progear').text(proGearWeight.toString());
-      $ppmEntitlementProgear.css('visibility', 'visible');
+      $progearAllowanceText.css('visibility', 'visible');
+      onWeightProgearInput(null);
 
       // The visibility of the spouse progear field is controlled elsewhere, so it is safe to make this label visible regardless
-      var proGearWeightSpouse = entitlement.pro_gear_weight_spouse > 0 ? entitlement.pro_gear_weight_spouse : 0;
+      var proGearWeightSpouse = getEntitlementProgearSpouse(entitlement);
       $('#entitlement_progear_spouse').text(proGearWeightSpouse.toString());
-      $ppmEntitlementProgearSpouse.css('visibility', 'visible');
+      $progearSpouseAllowanceText.css('visibility', 'visible');
+      onWeightProgearSpouseInput(null);
     }
   };
 
-  $rank.on('change', onPersonalInfoChanged);
-  $('#dependents_yes').on('change', onPersonalInfoChanged);
-  $('#dependents_no').on('change', onPersonalInfoChanged);
-  $('#married_yes').on('change', function() { $('#ppm-progear-spouse').removeAttr('hidden'); });
-  $('#married_no').on('change', function() { $('#ppm-progear-spouse').attr('hidden', true); });
+  var validateEntitlementField = function($input, getEntitlementFn) {
+    // an empty field is never an error
+    if ($input.val() == '')
+      return true;
 
+    // Some kinds of input validation can happen whether or not we know what the entitlement is
+    var weightLbs = parseInt($input.val());
+    if (weightLbs == NaN || weightLbs < 0)
+      return false;
+
+    // If the rank has not been chosen yet, then the entitlement cannot be loaded. Whatever the number is, it's fine for now
+    if (!$rank.val())
+      return true;
+
+    var entitlement = entitlementsJson.find(function(data) { return data.slug == $rank.val(); });
+    var entitlementWeight = getEntitlementFn(entitlement);
+    return weightLbs <= entitlementWeight;
+  };
+
+  var onWeightInput = function(e) {
+    if (validateEntitlementField($('#weight'), getEntitlementWeightSelf))
+      $('#weight-section').removeClass('usa-input-error');
+    else
+      $('#weight-section').addClass('usa-input-error');
+  };
+
+  var onWeightProgearInput = function(e) {
+    if (validateEntitlementField($('#weight_progear'), getEntitlementProgear))
+      $('#progear-section').removeClass('usa-input-error');
+    else
+      $('#progear-section').addClass('usa-input-error');
+  };
+
+  var onWeightProgearSpouseInput = function(e) {
+    if (validateEntitlementField($('#weight_progear_spouse'), getEntitlementProgearSpouse))
+      $('#progear-spouse-section').removeClass('usa-input-error');
+    else
+      $('#progear-spouse-section').addClass('usa-input-error');
+  };
+
+  $rank.on('change', onPersonalInfoChanged);
+  $('#dependents_yes').change(onPersonalInfoChanged);
+  $('#dependents_no').change(onPersonalInfoChanged);
+  $('#married_yes').change(function() { $('#progear-spouse-section').removeAttr('hidden'); });
+  $('#married_no').change(function() { $('#progear-spouse-section').attr('hidden', true); });
+
+  $('#weight').on('input', onWeightInput);
+  $('#weight_progear').on('input', onWeightProgearInput);
+  $('#weight_progear_spouse').on('input', onWeightProgearSpouseInput);
 })(window, jQuery);
 
 function isDigitKey(evt) {
