b08c0802b61bdc001b472064d26ed1f41700d302
Aileen Chen <aileen@dds.mil>
2017-11-14 15:28:15 -0500

    Add customer service information partial

--- app/controllers/service_specific_information_controller.rb
+++ app/controllers/service_specific_information_controller.rb
@@ -6,18 +6,23 @@ class ServiceSpecificInformationController < ApplicationController
     case params[:branch]
     when 'air-force'
       @posts = ServiceSpecificPost.where(branch: 'air_force')
+      @contact = BranchOfServiceContact.find_by(branch: 'Air Force')
       render 'air-force'
     when 'marine-corps'
       @posts = ServiceSpecificPost.where(branch: 'marine_corps')
+      @contact = BranchOfServiceContact.find_by(branch: 'Marine Corps')
       render 'marine-corps'
     when 'navy'
       @posts = ServiceSpecificPost.where(branch: 'navy')
+      @contact = BranchOfServiceContact.find_by(branch: 'Navy')
       render 'navy'
     when 'coast-guard'
       @posts = ServiceSpecificPost.where(branch: 'coast_guard')
+      @contact = BranchOfServiceContact.find_by(branch: 'Coast Guard')
     else
       # Fall back to army
       @posts = ServiceSpecificPost.where(branch: 'army')
+      @contact = BranchOfServiceContact.find_by(branch: 'Army')
       render 'army'
     end
   end
--- app/models/branch_of_service_contact.rb
+++ app/models/branch_of_service_contact.rb
@@ -12,4 +12,12 @@ class BranchOfServiceContact < ApplicationRecord
   def retiree_contact_info?
     retiree_contact_phone? || retiree_contact_fax? || retiree_email || retiree_post
   end
+
+  def customer_service_contact_info?
+    custsvc_dsn? || custsvc_tel_comm? || custsvc_tel_tollfree? || custsvc_email? || custsvc_url? || custsvc_facebook_url
+  end
+
+  def claims_contact_info?
+    claims_dsn? || claims_tel_comm? || claims_tel_tollfree? || claims_fax_dsn? || claims_fax_comm? || claims_fax_tollfree? || claims_email? || claims_post?
+  end
 end
