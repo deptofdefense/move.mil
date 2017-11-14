c577c7dae8eb2791a43e4b3df57ea41c75b17d4c
Aileen Chen <aileen@dds.mil>
2017-11-14 14:41:53 -0500

    Change routing for service-specific pages

--- app/controllers/service_specific_information_controller.rb
+++ app/controllers/service_specific_information_controller.rb
@@ -1,3 +1,25 @@
 class ServiceSpecificInformationController < ApplicationController
+<<<<<<< HEAD
   def index; end
+=======
+  def index
+    case params[:branch]
+    when 'air-force'
+      @posts = ServiceSpecificPost.where(branch: 'air_force')
+      render 'air-force'
+    when 'marine-corps'
+      @posts = ServiceSpecificPost.where(branch: 'marine_corps')
+      render 'marine-corps'
+    when 'navy'
+      @posts = ServiceSpecificPost.where(branch: 'navy')
+      render 'navy'
+    when 'coast-guard'
+      @posts = ServiceSpecificPost.where(branch: 'coast_guard')
+    else
+      # Fall back to Army
+      @posts = ServiceSpecificPost.where(branch: 'army')
+      render 'army'
+    end
+  end
+>>>>>>> Change routing for service-specific pages
 end
