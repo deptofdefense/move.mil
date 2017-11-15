8db7d1ebe0e269998fdd985e87b6d3e9e5e85d42
James Athey <james@dds.mil>
2017-11-15 17:43:09 -0500

    fix typo in shorthaul query

--- app/models/shorthaul.rb
+++ app/models/shorthaul.rb
@@ -1,6 +1,6 @@
 class Shorthaul < ApplicationRecord
   def self.rate(year, cwt, distance)
-    select(:rate).find_by('year = ? AND ? cwt_mi @> ?', year, cwt * distance).rate
+    select(:rate).find_by('year = ? AND cwt_mi @> ?', year, cwt * distance).rate
   end
 
   validates :year, :cwt_mi, :rate, presence: true
