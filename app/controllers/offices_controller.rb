f4ca00390a84f6aa1be5abca98f2498385b53e75
Jason Garber <jason@dds.mil>
2017-11-15 16:35:25 -0500

    Refactor index action avoiding AbcSize concerns

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,10 +1,10 @@
 class OfficesController < ApplicationController
   def index
     return unless search
-    return @error_message = search.error_message unless search.valid?
-    return redirect_to offices_path(search.result) if request.post?
+    return error_message unless search.valid?
+    return transportation_offices unless request.post?
 
-    @transportation_offices = TransportationOffice.by_distance_with_shipping_office(search.result).paginate(page: params[:page])
+    redirect_to offices_path(search.result)
   end
 
   private
@@ -13,12 +13,20 @@ class OfficesController < ApplicationController
     CoordinatesSearch.new(params) if params[:latitude].present? && params[:longitude].present?
   end
 
+  def error_message
+    @error_message ||= search.error_message
+  end
+
   def installation_search
     InstallationSearch.new(params) if params[:query].present?
   end
 
   def search
     @search ||= coordinates_search || zip_code_search || installation_search || nil
+  end
+
+  def transportation_offices
+    @transportation_offices ||= TransportationOffice.by_distance_with_shipping_office(search.result).paginate(page: params[:page])
   end
 
   def zip_code_search
