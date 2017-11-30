ff2e577a2c0daf419d7b45ca3446a32c1a8c2857
James Athey <james@dds.mil>
2017-11-30 12:59:30 -0500

    remove commented out code, as the design is expected to change with the future content

--- spec/requests/tutorials/index_spec.rb
+++ spec/requests/tutorials/index_spec.rb
@@ -13,25 +13,5 @@ RSpec.describe TutorialsController, type: :request do
         assert_template 'index'
       end
     end
-
-# rubocop:disable Style/BlockComments
-=begin
-    context 'when tutorials exist' do
-      let!(:tutorials) { create_list(:tutorial, 2) }
-
-      it 'displays a list of tutorials' do
-        get '/tutorials'
-
-        assert_select '.usa-accordion-bordered' do
-          assert_select 'li:nth-child(1)' do
-            assert_select '.usa-accordion-button', text: tutorials.first.title
-            assert_select '.carousel-item', length: 2
-          end
-
-          assert_select 'li:nth-child(2) .usa-accordion-button', text: tutorials.last.title
-        end
-      end
-    end
-=end
   end
 end
