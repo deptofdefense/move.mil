ea618a32a7789570c5d784a7cfda7cb16cf18e32
Aileen Chen <aileen@dds.mil>
2017-12-07 11:46:20 -0500

    Use high voltage + rails routes

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,11 +1,28 @@
 class SitemapController < ApplicationController
   def index
-    @pages = ['/', '/customer-service', '/entitlements', '/faqs', '/moving-guide/civilians', '/moving-guide/conus', '/moving-guide/nightmare-moves', '/moving-guide/oconus', '/moving-guide/reimbursements', '/moving-guide/retirees-separatees', '/moving-guide/tdy', '/moving-guide/tips', '/resources/inventory-form', '/resources/locator-maps', '/resources/weight-estimator', '/service-specific-information', '/tutorials']
+    all_urls = [high_voltage_urls, route_urls, service_specific_information_urls, tutorial_urls]
 
-    @branches_of_service = BranchOfService.all
-
-    respond_to do |format|
-      format.xml
+    @url_set = Set.new
+    all_urls.each do |urls|
+      @url_set.merge(urls)
     end
+  end
+
+  def high_voltage_urls
+    HighVoltage.page_ids.map { |page| page_url(page) if page != 'homepage' }.compact
+  end
+
+  def route_urls
+    routes = Rails.application.routes.routes
+    paths = routes.map { |route| route.path.spec.to_s[%r{^\/([^\(:*]+)}, 1] }.compact
+    paths.map { |path| page_url(path.chomp('/')) unless path.match?(/^(rails|assets|sitemap|homepage|\d)/) }.compact
+  end
+
+  def service_specific_information_urls
+    BranchOfService.all.map { |branch| service_specific_information_url(branch) }
+  end
+
+  def tutorial_urls
+    Tutorial.all.map { |tutorial| tutorial_url(tutorial) }
   end
 end
--- app/views/sitemap/index.xml.builder
+++ app/views/sitemap/index.xml.builder
@@ -1,17 +1,8 @@
-base_url = 'http://www.move.mil'
-
 xml.instruct! :xml, version: '1.0'
-xml.urlset 'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9' do
-  @pages.each do |page|
+xml.urlset xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9' do
+  @url_set.each do |url|
     xml.url do
-      xml.loc base_url + page
-      xml.changefreq 'weekly'
-    end
-  end
-
-  @branches_of_service.each do |branch|
-    xml.url do
-      xml.loc base_url + service_specific_information_path(branch)
+      xml.loc url
       xml.changefreq 'weekly'
     end
   end
