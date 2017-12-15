f6af04e1dbbbe515791087248a2f1d942a4452b1
Jason Garber <jason@dds.mil>
2017-12-14 17:56:28 -0800

    Rework associations based on RuboCop feedback

--- app/models/shipping_office.rb
+++ app/models/shipping_office.rb
@@ -1,3 +1,3 @@
 class ShippingOffice < Office
-  has_many :transportation_offices, class_name: 'Office', foreign_key: :shipping_office_id, dependent: :destroy
+  has_many :transportation_offices, dependent: :destroy
 end
--- app/models/transportation_office.rb
+++ app/models/transportation_office.rb
@@ -1,5 +1,7 @@
 class TransportationOffice < Office
-  belongs_to :shipping_office, class_name: 'Office', optional: true
+  # rubocop:disable Rails/InverseOf
+  belongs_to :shipping_office, foreign_key: :shipping_office_id, optional: true
+  # rubocop:enable Rails/InverseOf
 
   def self.by_distance_with_shipping_office(latitude:, longitude:)
     includes(:shipping_office).by_distance(origin: [latitude, longitude])
