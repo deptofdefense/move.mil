e7332d578778a9707608a5c1d0e1cb12c709ef40
Aileen Chen <aileen@dds.mil>
2017-12-18 16:24:20 -0500

    Make DB queries better

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,6 @@
 class TutorialsController < ApplicationController
   def show
-    @tutorials = Tutorial.includes(:tutorial_steps).all
+    @tutorials = Tutorial.includes(:tutorial_steps).all.to_a
     @tutorial = params[:id] ? @tutorials.find { |tutorial| tutorial.slug == params[:id] } : @tutorials.first
   end
 end
