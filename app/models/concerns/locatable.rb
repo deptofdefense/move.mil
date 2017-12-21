01f5cf0898108e4cfdf18f6953f2ea053e661489
Jason Garber <jason@dds.mil>
2017-12-21 11:58:57 -0500

    Remove acts_as_mappable from Locatable concern
    Models mixing in this concern don't have latitude or longitude columns!

--- app/models/concerns/locatable.rb
+++ app/models/concerns/locatable.rb
@@ -2,8 +2,6 @@ module Locatable
   extend ActiveSupport::Concern
 
   included do
-    acts_as_mappable lat_column_name: :latitude, lng_column_name: :longitude
-
     has_one :location, as: :locatable, dependent: :destroy, inverse_of: :locatable
 
     accepts_nested_attributes_for :location
