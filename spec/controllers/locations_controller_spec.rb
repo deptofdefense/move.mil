f5f27e45fc1da2d10fff1ce4f8a65b1d1ab74ac5
Jason Garber <jason@dds.mil>
2017-12-21 09:52:01 -0500

    Add controller spec to round out test coverage

---
+++ spec/controllers/locations_controller_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe LocationsController, type: :controller do
+  describe 'POST #index' do
+    context 'when performing an installation search' do
+      it 'assigns @search' do
+        post :index, params: { query: 'installation' }
+
+        expect(assigns(:search).query).to eq('installation')
+      end
+    end
+
+    context 'when performing a ZIP code search' do
+      context 'with a zero-prefixed query' do
+        it 'assigns @search' do
+          post :index, params: { query: '601' }
+
+          expect(assigns(:search).query).to eq('00601')
+        end
+      end
+
+      context 'with a non-zero-prefixed query' do
+        it 'assigns @search' do
+          post :index, params: { query: '20010' }
+
+          expect(assigns(:search).query).to eq('20010')
+        end
+      end
+
+    end
+  end
+end
