0c50bda977ac534d2b5e501017c89c6eddd5a577
Aileen Chen <aileen@dds.mil>
2017-12-22 11:26:16 -0500

    Update spec, fix status

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,4 @@
 class ErrorsController < ApplicationController
-
   before_action :set_format
 
   def not_found
@@ -7,10 +6,11 @@ class ErrorsController < ApplicationController
   end
 
   def internal_server_error
-    render file: Rails.public_path.join('500.html'), layout: false
+    render file: Rails.public_path.join('500.html'), status: 500, layout: false
   end
 
   private
+
   def set_format
     request.format = :html
   end
--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -1,16 +1,47 @@
 RSpec.describe ErrorsController, type: :request do
+  def rails_respond_without_detailed_exceptions
+    env_config = Rails.application.env_config
+    original_show_exceptions = env_config['action_dispatch.show_exceptions']
+    original_show_detailed_exceptions = env_config['action_dispatch.show_detailed_exceptions']
+    env_config['action_dispatch.show_exceptions'] = true
+    env_config['action_dispatch.show_detailed_exceptions'] = false
+    yield
+  ensure
+    env_config['action_dispatch.show_exceptions'] = original_show_exceptions
+    env_config['action_dispatch.show_detailed_exceptions'] = original_show_detailed_exceptions
+  end
+
   describe 'GET #not_found' do
     before do
-      get '/404'
+      rails_respond_without_detailed_exceptions do
+        get '/page_that_doesnt_exist.pdf'
+      end
     end
 
-    it 'returns HTTP success status code' do
+    it 'returns HTTP 404 status code' do
       expect(response).to have_http_status(:not_found)
     end
 
     it 'renders the not found template' do
       assert_template 'not_found'
       assert_select '.not-found-message h1', text: 'Oops...'
+    end
+  end
+
+  describe 'GET #internal_server_error' do
+    before do
+      expect(Entitlement).to receive(:all).and_raise('kaboom!')
+      rails_respond_without_detailed_exceptions do
+        get '/entitlements'
+      end
+    end
+
+    it 'returns HTTP 500 status code' do
+      expect(response).to have_http_status(:internal_server_error)
+    end
+
+    it 'renders the default rails error page' do
+      assert_select '.rails-default-error-page h1', text: 'We\'re sorry, but something went wrong.'
     end
   end
 end
