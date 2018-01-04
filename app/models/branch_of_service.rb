5dd34ed149b80b15fe2e3a6b552d2ea363c54057
Jason Garber <jason@dds.mil>
2018-01-04 16:30:25 -0500

    Add accepts_nested_attributes_for to relevant models

--- app/models/branch_of_service.rb
+++ app/models/branch_of_service.rb
@@ -6,6 +6,8 @@ class BranchOfService < ApplicationRecord
   has_one :branch_of_service_contact, dependent: :destroy
   has_many :service_specific_posts, dependent: :destroy
 
+  accepts_nested_attributes_for :branch_of_service_contact, :service_specific_posts
+
   validates :name, :display_order, presence: true
 
   default_scope { order(display_order: :asc) }
