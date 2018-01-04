a75f715599465429f7ce19824a556cb520ffb30d
Aileen Chen <aileen@dds.mil>
2018-01-04 14:34:47 -0500

    Create transportation office once in location spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,17 +1,19 @@
 RSpec.describe Location, type: :model do
+  let(:transportation_office) { TransportationOffice.new(name: 'Office') }
+
   it 'is valid with valid coordinates' do
-    expect(Location.new(latitude: 90, longitude: 180, locatable: TransportationOffice.new(name: 'Office'))).to be_valid
-    expect(Location.new(latitude: -90, longitude: -180, locatable: TransportationOffice.new(name: 'Office'))).to be_valid
-    expect(Location.new(latitude: 0, longitude: 0, locatable: TransportationOffice.new(name: 'Office'))).to be_valid
+    expect(Location.new(latitude: 90, longitude: 180, locatable: transportation_office)).to be_valid
+    expect(Location.new(latitude: -90, longitude: -180, locatable: transportation_office)).to be_valid
+    expect(Location.new(latitude: 0, longitude: 0, locatable: transportation_office)).to be_valid
   end
 
   it 'is not valid with an invalid latitude' do
-    expect(Location.new(latitude: 91, longitude: 180, locatable: TransportationOffice.new(name: 'Office'))).to_not be_valid
-    expect(Location.new(latitude: -91, longitude: 180, locatable: TransportationOffice.new(name: 'Office'))).to_not be_valid
+    expect(Location.new(latitude: 91, longitude: 180, locatable: transportation_office)).to_not be_valid
+    expect(Location.new(latitude: -91, longitude: 180, locatable: transportation_office)).to_not be_valid
   end
 
   it 'is not valid with an invalid longitude' do
-    expect(Location.new(latitude: 90, longitude: 181, locatable: TransportationOffice.new(name: 'Office'))).to_not be_valid
-    expect(Location.new(latitude: 90, longitude: -181, locatable: TransportationOffice.new(name: 'Office'))).to_not be_valid
+    expect(Location.new(latitude: 90, longitude: 181, locatable: transportation_office)).to_not be_valid
+    expect(Location.new(latitude: 90, longitude: -181, locatable: transportation_office)).to_not be_valid
   end
 end
