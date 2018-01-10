2c75e02db723d032491f9fc0780acf63f9f6fd82
Jason Garber <jason@dds.mil>
2018-01-10 15:57:20 -0500

    Add additional system specs

--- spec/system/entitlements_spec.rb
+++ spec/system/entitlements_spec.rb
@@ -1,18 +1,28 @@
-RSpec.describe 'Entitlements', type: :system do
-  describe 'GET #index', js: true do
+RSpec.describe EntitlementsController, type: :system do
+  describe 'GET #index' do
     let!(:entitlement) { create(:entitlement) }
 
-    before do
+    before(:each) do
       visit '/entitlements'
+    end
 
+    it 'displays a list of ranks' do
+      expect(page).to have_selector('#entitlements-rank-list')
+      expect(page).not_to have_selector('#entitlements-search')
+    end
+
+    it 'displays an entitlements search form', js: true do
+      expect(page).to have_selector('#entitlements-search')
+      expect(page).not_to have_selector('#entitlements-rank-list')
+    end
+
+    it 'displays entitlements based on user input', js: true do
       select 'E-1', from: :rank
       select 'Yes, I have dependents', from: :dependency_status
       select 'Within the Continental United States (CONUS)', from: :move_type
 
       click_button 'View Results'
-    end
 
-    it 'displays entitlements based on user input' do
       expect(page).to have_selector('#entitlements-search-results') do |content|
         expect(content).to have_text('Military Pay Grade: E-1')
         expect(content).to have_text('Dependency Status: Yes, I have dependents (spouse/children) that are authorized to move')
