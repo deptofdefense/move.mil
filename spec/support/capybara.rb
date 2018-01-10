a3359323f091e9669fef7cb276df521311804be3
Jason Garber <jason@dds.mil>
2018-01-10 16:36:36 -0500

    Move before() config to rails_helper

--- spec/support/capybara.rb
+++
@@ -1,9 +0,0 @@
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
-  end
-end
