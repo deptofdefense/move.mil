9d72be7f205a796fcc5692dbf8083fa1f48fb37e
Aileen Chen <aileen@dds.mil>
2018-01-11 15:23:13 -0500

    Use .nil? predicate

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -3,7 +3,7 @@ class TutorialsController < ApplicationController
     @tutorials = Tutorial.includes(:tutorial_steps).all.to_a
     if params[:id]
       @tutorial = @tutorials.find { |tutorial| tutorial.slug == params[:id] }
-      return unless @tutorial == nil
+      return unless @tutorial.nil?
 
       # Redirect to /tutorials on invalid / outdated tutorial ID
       redirect_to tutorial_path
