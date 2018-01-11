35dc3409699221119c69d659a1782a56a7a32311
Aileen Chen <aileen@dds.mil>
2018-01-11 15:17:57 -0500

    Redirect to /tutorials for bad tutorial IDs

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,14 @@
 class TutorialsController < ApplicationController
   def show
     @tutorials = Tutorial.includes(:tutorial_steps).all.to_a
-    @tutorial = params[:id] ? @tutorials.find { |tutorial| tutorial.slug == params[:id] } : @tutorials.first
+    if params[:id]
+      @tutorial = @tutorials.find { |tutorial| tutorial.slug == params[:id] }
+      return unless @tutorial == nil
+
+      # Redirect to /tutorials on invalid / outdated tutorial ID
+      redirect_to tutorial_path
+    else
+      @tutorial = @tutorials.first
+    end
   end
 end
