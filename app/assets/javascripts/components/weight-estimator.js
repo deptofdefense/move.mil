0d79ad42777fdfb804a611d9a721ece3111c3e88
Jason Garber <jason@dds.mil>
2018-03-19 09:25:21 -0400

    Refactor Weight Estimator JS

--- app/assets/javascripts/components/weight-estimator.js
+++ app/assets/javascripts/components/weight-estimator.js
@@ -1,85 +1,87 @@
 (function(window, $) {
   'use strict';
 
-  var CoordinatesSearchForm = function(options) {
-    this.$outerContainer = options.$outerContainer;
-    this.$buttons = options.$buttons;
+  var WeightEstimator = function(options) {
+    this.$container = options.$container;
+    this.$categoryButtons = options.$categoryButtons;
+    this.$estimateButton = options.$estimateButton;
     this.$weightResult = options.$weightResult;
 
     this.setup();
   };
 
-  CoordinatesSearchForm.prototype = {
-    calculateWeight: function($container) {
-      var sum = 0;
+  WeightEstimator.prototype = {
+    events: {
+      clickCategoryButton: function(event) {
+        event.preventDefault();
 
-      var $quantityInputs = $container.find('.hhg-quantity-input');
+        var $button = $(event.target),
+            subtotal = this.calculateWeight($button.closest('.hhg-category')),
+            total = this.calculateWeight(this.$container),
+            categoryKey = $button.attr('id');
+
+        $('#' + categoryKey + '_subtotal').val(subtotal);
+        $('#' + categoryKey + '_total').val(total);
+      },
+
+      clickEstimateButton: function(event) {
+        event.preventDefault();
+
+        var total = this.calculateWeight(this.$container),
+            packingDays = '4 days';
+
+        if (total < 5000) {
+          packingDays = '1 day';
+        } else if (total < 7000) {
+          packingDays = '2 days';
+        } else if (total < 9000) {
+          packingDays = '3 days';
+        }
+
+        this.$weightResult.html(
+          '<div>Estimated grand total: ' + total + ' lbs</div>' +
+          '<div>Average time to pack ' + total + ' lbs is ' + packingDays + '</div>'
+        );
+      }
+    },
+
+    calculateWeight: function($el) {
+      var sum = 0,
+          $quantityInputs = $el.find('.hhg-quantity-input'),
+          $weightInputs = $el.find('.hhg-weight-input');
+
       $quantityInputs.each(function() {
-        var $input = $(this);
-        if ($input.val() > 0) {
-          var $weightInput = $('#' + this.id + '_weight');
-          sum += $input.val() * $weightInput.val();
+        var val = parseInt($(this).val());
+
+        if (val > 0) {
+          sum += val * $('#' + this.id + '_weight').val();
         }
       });
 
-      var $weightInputs = $container.find('.hhg-weight-input');
       $weightInputs.each(function() {
-        var $input = $(this);
-        if ($input.val() > 0) {
-          sum += parseInt($input.val());
+        var val = parseInt($(this).val());
+
+        if (val > 0) {
+          sum += val;
         }
       });
 
       return sum;
     },
 
-    events: {
-      click: function(event) {
-        event.preventDefault();
-
-        var $button = $(event.target);
-        var total = this.calculateWeight(this.$outerContainer);
-
-        var isSubtotalButton = $button.hasClass('category-calculate-button');
-        if (isSubtotalButton) {
-          var $categoryContainer = $button.closest('.hhg-category');
-          var subtotal = this.calculateWeight($categoryContainer);
-
-          var categoryKey = $button.attr('id');
-          var $subtotalInput = $categoryContainer.find('#' + categoryKey + '_subtotal');
-          var $totalInput = $categoryContainer.find('#' + categoryKey + '_total');
-
-          $subtotalInput.val(subtotal);
-          $totalInput.val(total);
-        } else {
-          var packing_days = '4 days';
-          if (total < 5000) {
-            packing_days = '1 day';
-          } else if (total < 7000) {
-            packing_days = '2 days';
-          } else if (total < 9000) {
-            packing_days = '3 days';
-          }
-          this.$weightResult.html(
-            '<div>Estimated grand total: ' + total + ' lbs</div><div>Average time to pack ' + total + ' lbs is ' + packing_days + '</div>'
-          );
-        }
-      }
-    },
-
     setup: function() {
-      for (var i = 0; i < this.$buttons.length; i++) {
-        $(this.$buttons[i]).on('click', this.events.click.bind(this));
-      }
+      this.$categoryButtons.on('click', this.events.clickCategoryButton.bind(this));
+      this.$estimateButton.on('click', this.events.clickEstimateButton.bind(this));
     }
   };
 
-  var $outerContainer = $('#weight-estimator');
+  var $container = $('#weight-estimator');
 
-  if ($outerContainer.length) {
-    new CoordinatesSearchForm({
-      $outerContainer: $outerContainer,
-      $buttons: $outerContainer.find('button'),
+  if ($container.length) {
+    new WeightEstimator({
+      $categoryButtons: $container.find('.category-calculate-button'),
+      $estimateButton: $('#calculate-button'),
+      $container: $container,
       $weightResult: $('#weight-result')
     });
   }
