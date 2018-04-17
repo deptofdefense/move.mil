b7b4f4ed7f98483db2a86b02390eadaaca118f95
Jason Garber <jason@dds.mil>
2018-04-17 10:35:35 -0400

    Implement RuboCop recommendations

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,11 +2,11 @@ class ErrorsController < ApplicationController
   before_action :set_format
 
   def not_found
-    render status: 404
+    render status: :not_found
   end
 
   def internal_server_error
-    render file: Rails.public_path.join('500.html'), status: 500, layout: false
+    render file: Rails.public_path.join('500.html'), status: :internal_server_error, layout: false
   end
 
   private
